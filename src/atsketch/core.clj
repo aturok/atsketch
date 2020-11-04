afae9619e6cfec61a06f72bb80871283157c1758
Alex Turok <chaot8@gmail.com>
2020-11-04 03:20:58 +0300

    other background

--- src/atsketch/core.clj
+++ src/atsketch/core.clj
@@ -219,40 +219,62 @@
         gen-dims #(sort [(gen-size) (gen-size)])
 
         backs (repeatedly (* 10 w) (fn []
-                                    {:coords {:x (* (rand) w)
-                                              :y (* (rand) h)
-                                              :w 2
-                                              :h 2}
-                                     :color {:h (random-cl 154 10 0 255)
-                                             :s 150
-                                             :b 100
-                                             :a (random-cl 20 30 0 255)}}))
-        
-        top-band-h (* (float (/ h 3)))
-        top-band-center 50        
+                                     {:coords {:x (* (rand) w)
+                                               :y (* (rand) h)
+                                               :w 2
+                                               :h 2}
+                                      :color {:h (random-cl 154 10 0 255)
+                                              :s 150
+                                              :b 100
+                                              :a (random-cl 20 30 0 255)}}))
+
+        gen-y (fn [x-around x-value y-center y-range]
+                (let [proximity (- 1 (/ (Math/abs (- x-value x-around)) w))]
+                  (random-c (* y-center proximity) (* y-range (+ proximity 0.05)))))
+
+        band-h (* (float (/ h 3)))
+        top-band-center 50
         n-tops (int (* 0.7 w))
         tops (vec (repeatedly n-tops (fn []
-                                       (let [[sw sh] (gen-dims)]
-                                         {:coords {:x (random-c (* w 0.30) (/ w 4))
-                                                   :y (random-c top-band-center (/ top-band-h 2))
+                                       (let [[sw sh] (gen-dims)
+                                             x (random-c (* w 0.30) (/ w 4))]
+                                         {:coords {:x x
+                                                   :y (gen-y (* w 0.30) x top-band-center (/ band-h 2))
                                                    :w sw
                                                    :h sh}
                                           :color {:h (random-cl 235 10 0 255) :s 255 :b 255
                                                   :a (random-cl 150 50 0 255)}}))))
-        
-        low-band-h (* (float (/ h 3)))
-        low-band-center (- h 80)
+
+        tops-backs (vec (repeatedly (* 3 n-tops) (fn []
+                                                   {:coords {:x (random-c (* w 0.30) (/ w 4))
+                                                             :y (random-c top-band-center band-h)
+                                                             :w 1
+                                                             :h 1}
+                                                    :color {:h (random-cl 235 10 0 255) :s 255 :b 255
+                                                            :a (random-cl 50 50 0 255)}})))
+
+        low-band-center 200
         n-bots (int (* 1 w))
         bottoms (vec (repeatedly n-bots (fn []
-                                          (let [[sw sh] (gen-dims)]
-                                            {:coords {:x (random-c (* w 0.70) (/ w 4))
-                                                      :y (random-c low-band-center (/ low-band-h 2))
+                                          (let [[sw sh] (gen-dims)
+                                                x (random-c (* w 0.70) (/ w 4))]
+                                            {:coords {:x x
+                                                      :y (- h (gen-y (* w 0.70) x low-band-center (/ band-h 2)))
                                                       :w sw
                                                       :h sh}
                                              :color {:h (random-cl 154 10 0 255) :s 255 :b 255
                                                      :a (random-cl 150 75 0 255)}}))))
+        bots-backs (vec (repeatedly (* 4 n-tops) (fn []
+                                                   {:coords {:x (random-c (* w 0.70) (/ w 4))
+                                                             :y (- h (random-c low-band-center band-h))
+                                                             :w 1
+                                                             :h 1}
+                                                    :color {:h (random-cl 154 10 0 255) :s 255 :b 255
+                                                            :a (random-cl 50 50 0 255)}})))
         xxx :xxx]
-    (concat backs
+    (concat ;backs
+            tops-backs
+            bots-backs
             bottoms
             tops)))
 
