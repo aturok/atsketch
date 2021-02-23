a90627ef65e47fac20367de7160fc449a25996de
Alex Turok <chaot8@gmail.com>
2021-02-23 22:55:38 +0300

    multiple images with save

--- src/atsketch/core.clj
+++ src/atsketch/core.clj
@@ -7,7 +7,20 @@
             [atsketch.draw :as d]))
 
 (def w 1000)
-(def h 1000)
+(def h 5000)
+
+(defn go-next-frame [{:keys [frame max-frame w h] :as state}]
+  (if (>= frame max-frame)
+    (assoc state
+           :fram max-frame
+           :done true
+           :curves [])
+    (assoc state
+           :frame (inc frame)
+           :curves (do
+                     (q/random-seed frame)
+                     (concat (crv/fat-curves w h)
+                             (crv/some-curves w h))))))
 
 (defn update-state [state] state)
 
@@ -18,21 +31,24 @@
   (q/color-mode :hsb)
   ; setup function returns initial state. It contains
   ; circle color and position.
-  {:color {:h 4 :s 0 :b 255}
-   :rects (sq2/tris1 w h)
-   :curves (do
-             (q/random-seed 10)
-             (concat (crv/fat-curves w h)
-                     (crv/some-curves w h)))})
+  {:frame 0
+   :max-frame 10
+   :done false
+   :w w
+   :h h
+   :color {:h 4 :s 0 :b 255}
+   :curves []})
 
 (defn settings []
   (q/smooth 100))
 
-
-
-(defn draw-state [{:keys [curves]}]
-  (q/background 25 10 255)
-  (doall (map d/draw-bezier curves)))
+(defn draw-state [{:keys [curves done frame w h]}]
+  (if done
+    (q/background 0)
+    (let [g (q/create-graphics w h)]
+      (q/background 25 10 255)
+      (doall (map d/draw-bezier curves))
+      (q/save (str "/out/livingroom/" (format "%04d" frame) ".png")))))
 
 ;; 400 0 300 200 540 500 400 1000
 
@@ -47,7 +63,7 @@
   :setup setup
   :settings settings
   ; update-state is called on each iteration before draw-state.
-  :update update-state
+  :update go-next-frame
   :draw draw-state
   :mouse-clicked mouse-press
 ;   :features [:keep-on-top]
@@ -55,3 +71,11 @@
   ; Check quil wiki for more info about middlewares and particularly
   ; fun-mode.
   :middleware [m/fun-mode])
+
+;; create 5 renders with different seeds
+;; save to files
+;; in the specified folder
+;; 
+;; 
+;; start small:
+;; just use create graphics, with graphics and save to save
