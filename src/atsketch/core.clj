d5e6a6679f88fd949fc96d482f60e8dc9e84b6e3
Alex Turok <chaot8@gmail.com>
2021-05-02 13:12:14 +0300

    sun

--- src/atsketch/core.clj
+++ src/atsketch/core.clj
@@ -4,25 +4,46 @@
             [atsketch.shapes :as sh]
             [atsketch.squares2 :as sq2]
             [atsketch.curves :as crv]
+            [atsketch.util :as util :refer [random-c random-cl]]
             [atsketch.draw :as d]))
 
-(def w 1000)
-(def h 4000)
+(def w 8000)
+(def h 8000)
 
-(defn go-next-frame [{:keys [frame max-frame w h] :as state}]
+(defn rainbow-color [ncols col row]
+  {:h (* 255 (/ col ncols)) :s 200 :b 255 :a (- 250 (* 10 row))})
+
+(defn gen-circle-squares []
+  (let [n 550
+        size 14
+        displ (* 1.25 size)
+        skip 14
+        nits 30
+        alpha-degrade 5
+        r 1600]
+    (vec
+     (for [i (range n)]
+       (let [basecolor {:h (+ 18 (random-c 0 8)) :s 180 :b 255 :a 250}]
+         (vec (for [j (when (not= (int (/ skip 2)) (mod i skip))
+                        (range (random-cl (* nits 0.5) (* nits 0.25) 1 (* 1.5 nits))))]
+                {:coords {:x 0
+                          :y (+ r (* j displ))
+                          :w size
+                          :h size}
+                 :color  (update basecolor :a #(- % (+ (* alpha-degrade j) (random-c 0 20))))})))))))
+
+(defn go-next-frame [{:keys [frame max-frame] :as state}]
   (if (>= frame max-frame)
     (assoc state
-           :fram max-frame
+           :frame max-frame
            :done true
-           :curves [])
+           :circle-squares [])
     (assoc state
            :frame (inc frame)
-           :curves (do
-                     (q/random-seed frame)
-                     (concat (crv/fat-curves w h)
-                             (crv/some-curves w h))))))
+           :circle-squares (gen-circle-squares))))
 
-(defn update-state [state] state)
+
+(defn update-state [state] go-next-frame)
 
 (defn setup []
   ; Set frame rate to 30 frames per second.
@@ -34,6 +55,7 @@
   {:frame 0
    :max-frame 100
    :done false
+   :circle-squares (gen-circle-squares)
    :w w
    :h h
    :color {:h 4 :s 0 :b 255}
@@ -42,13 +64,19 @@
 (defn settings []
   (q/smooth 100))
 
-(defn draw-state [{:keys [curves done frame w h]}]
+(defn draw-state [{:keys [circle-squares frame done w h]}]
   (if done
     (q/background 0)
     (let [g (q/create-graphics w h)]
       (q/background 25 10 255)
-      (doall (map d/draw-bezier curves))
-      (q/save (str "/out/livingroom/" (format "%04d" frame) ".png")))))
+      (let [cx (/ w 2) cy (/ h 2)
+            da (/ (* 2 q/PI) (count circle-squares))]
+        (q/translate cx cy)
+        (doseq [row circle-squares]
+          (doseq [s row]
+            (d/draw-rect s))
+          (q/rotate da)))
+      (q/save (str "/out/sun/" (format "%04d" frame) ".png")))))
 
 ;; 400 0 300 200 540 500 400 1000
 
